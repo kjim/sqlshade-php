ae44e5c9040007c51975380da40e03997b475fd7
Keiji Muraishi <keiji.muraishi@gmail.com>
2010-05-31 15:09:01 +0900

    add node abstract class

---
+++ lib/SQLShade/Node.php
@@ -0,0 +1,29 @@
+<?php
+
+abstract class SQLShade_Node {
+
+    protected
+        $lineno,
+        $tag
+        ;
+
+    public function __construct($lineno, $tag = null) {
+        $this->lineno = $lineno;
+        $this->tag = $tag;
+    }
+
+    public function __toString() {
+        return get_class($this).'()';
+    }
+
+    public function getLine() {
+        return $this->lineno;
+    }
+
+    public function getNodeTag() {
+        return $this->tag;
+    }
+
+    abstract public function compile($compiler);
+
+}
