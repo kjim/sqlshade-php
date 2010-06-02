5b836d2107e027d7d0d4ac1a891177f07f0657c9
Keiji Muraishi <keiji.muraishi@gmail.com>
2010-06-02 17:36:49 +0900

    test the printer for index parameterized class

---
+++ lib/SQLShade/Printer/Index.php
@@ -0,0 +1,25 @@
+<?php
+
+class SQLShade_Printer_Index {
+
+    protected $buf;
+    protected $bound;
+
+    public function __construct() {
+        $this->buf = '';
+        $this->bound = array();
+    }
+
+    public function write($fragment) {
+        $this->buf .= $fragment;
+    }
+
+    public function bind($variable) {
+        $this->bound[] = $variable;
+    }
+
+    public function freeze() {
+        return array(&$buf, &$bound);
+    }
+
+}
---
+++ test/test_sqlshade_printer_index.php
@@ -0,0 +1,13 @@
+<?php
+require_once(dirname(__FILE__).'/bootstrap.php');
+require_once(dirname(__FILE__).'/../lib/SQLShade/Printer/Index.php');
+
+$t = new lime_test();
+
+// @test
+$printer = new SQLShade_Printer_Index();
+$printer->write("SELECT * FROM t_table WHERE t_table.status = ?");
+$printer->bind("1");
+list($buf, $bound) = $printer->freeze();
+$t->is($query, "SELECT * FROM t_table WHERE t_table.status = ?");
+$t->is($bound, array(1));
